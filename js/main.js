4a3e917aacaa8bfb23d104df748f4d9288b547e5
Dmytro Borodin <48290702+DmytroBorodin@users.noreply.github.com>
2020-09-15 21:24:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,57 @@
+'use strict'
+
+let pagesArr = [...document.querySelectorAll('.page')];
+
+let mainBtnsArray = [...document.querySelectorAll('.main__button')];
+
+let iconBtnsArray = [...document.querySelectorAll('.icon__button')];
+
+let backBtnsArray = [...document.querySelectorAll('.back__icon')];
+
+let lastPageBtns = [...pagesArr[pagesArr.length-1].querySelectorAll('button')];
+console.log(lastPageBtns);
+
+let switchPage = (el) => {
+	pagesArr.forEach(page => {
+		page.classList.add('disabled');
+	})
+	el.classList.remove('disabled');
+}
+
+mainBtnsArray.forEach(m_btn => {
+	m_btn.addEventListener('mousedown', () => {
+		m_btn.classList.add('clicked');
+	})
+	m_btn.addEventListener('mouseup', () => {
+		let pageToSwitch = +m_btn.getAttribute('page');
+		m_btn.classList.remove('clicked');
+		switchPage(pagesArr[pageToSwitch]);
+	})
+});
+
+iconBtnsArray.forEach(i__btn => {
+	i__btn.addEventListener('mousedown', () => {
+		i__btn.classList.add('clicked');
+	})
+	i__btn.addEventListener('mouseup', () => {
+		let pageToSwitch = +i__btn.getAttribute('page');
+		i__btn.classList.remove('clicked');
+		switchPage(pagesArr[pageToSwitch]);
+	})
+});
+
+lastPageBtns.forEach(l_btn => {
+	l_btn.addEventListener('mousedown', () => {
+		l_btn.classList.add('clicked');
+	});
+	l_btn.addEventListener('mouseup', () => {
+		l_btn.classList.remove('clicked');
+	});
+});
+
+backBtnsArray.forEach(b_btn => {
+	b_btn.addEventListener('click', () => {
+		let pageToSwitch = backBtnsArray.indexOf(b_btn);
+		switchPage(pagesArr[pageToSwitch]);
+	})
+})
